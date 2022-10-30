5d4559664699cbcf93678c3aa173685bc8342d3e
Alexander EG#9070 <116421926+AlexEG@users.noreply.github.com>
2022-10-30 20:57:04 +0200

    test
    testing different  ways to add an Active State to the section in viewport

--- js/app.js
+++ js/app.js
@@ -28,6 +28,56 @@ navigationMenu();
 //----testing------//
 let active = document.getElementsByClassName('your-active-class');
 
-window.addEventListener('scroll', function () {
-  console.log('test');
-});
+function update() {
+  const container = document.getElementById('controls');
+  const elem = document.getElementById('example');
+  const rect = elem.getBoundingClientRect();
+
+  container.innerHTML = '';
+  for (const key in rect) {
+    if (typeof rect[key] !== 'function') {
+      let para = document.createElement('p');
+      para.textContent = `${key} : ${rect[key]}`;
+      container.appendChild(para);
+    }
+  }
+}
+
+document.addEventListener('scroll', update);
+update();
+
+//ale the section are here
+sections;
+
+const div = document.createElement('div');
+div.className = 'foo';
+
+// our starting state: <div class="foo"></div>
+console.log(div.outerHTML);
+
+// use the classList API to remove and add classes
+div.classList.remove('foo');
+div.classList.add('anotherclass');
+
+// <div class="anotherclass"></div>
+console.log(div.outerHTML);
+
+// if visible is set remove it, otherwise add it
+div.classList.toggle('visible');
+
+// add/remove visible, depending on test conditional, i less than 10
+div.classList.toggle('visible', i < 10);
+
+console.log(div.classList.contains('foo'));
+
+// add or remove multiple classes
+div.classList.add('foo', 'bar', 'baz');
+div.classList.remove('foo', 'bar', 'baz');
+
+// add or remove multiple classes using spread syntax
+const cls = ['foo', 'bar'];
+div.classList.add(...cls);
+div.classList.remove(...cls);
+
+// replace class "foo" with class "bar"
+div.classList.replace('foo', 'bar');
