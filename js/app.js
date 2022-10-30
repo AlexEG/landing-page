71360043e38af6c6d25320fa95c24fb90a16fd1a
Alexander EG#9070 <116421926+AlexEG@users.noreply.github.com>
2022-10-30 21:14:59 +0200

    refactor
    testing

--- js/app.js
+++ js/app.js
@@ -26,58 +26,80 @@ navigationMenu();
 
 //strart with stor the avtive class in a var
 //----testing------//
-let active = document.getElementsByClassName('your-active-class');
 
-function update() {
-  const container = document.getElementById('controls');
-  const elem = document.getElementById('example');
-  const rect = elem.getBoundingClientRect();
+//all the section are here
 
-  container.innerHTML = '';
-  for (const key in rect) {
-    if (typeof rect[key] !== 'function') {
-      let para = document.createElement('p');
-      para.textContent = `${key} : ${rect[key]}`;
-      container.appendChild(para);
-    }
-  }
-}
+/*
+what we need to to 
+1.we  need to know if the section is in the viewport or not.
+2.and than we need to give this section the [ your-active-class ] 
+ so the css go to that section and give ti the color and stuff .
+3. we need to remove the [ your-active-class ] the rest of sections that are not in the viewport.
+*/
 
-document.addEventListener('scroll', update);
-update();
+let test5 = document.getElementById('section1');
 
-//ale the section are here
-sections;
+// Get it's position in the viewport
+let position = test5.getBoundingClientRect();
 
-const div = document.createElement('div');
-div.className = 'foo';
+// Log the results
+console.log(position);
 
-// our starting state: <div class="foo"></div>
-console.log(div.outerHTML);
+/* position of section1
+bottom:1128.1875  height:663.1875  left:16
+right:1565  top:465  width:1549  x:16  y:465
+*/
 
-// use the classList API to remove and add classes
-div.classList.remove('foo');
-div.classList.add('anotherclass');
+// const div = document.createElement('div');
+// div.className = 'foo';
 
-// <div class="anotherclass"></div>
-console.log(div.outerHTML);
+// // our starting state: <div class="foo"></div>
+// console.log(div.outerHTML);
 
-// if visible is set remove it, otherwise add it
-div.classList.toggle('visible');
+// // use the classList API to remove and add classes
+// div.classList.remove('foo');
+// div.classList.add('anotherclass');
 
-// add/remove visible, depending on test conditional, i less than 10
-div.classList.toggle('visible', i < 10);
+// // <div class="anotherclass"></div>
+// console.log(div.outerHTML);
 
-console.log(div.classList.contains('foo'));
+// // if visible is set remove it, otherwise add it
+// div.classList.toggle('visible');
 
-// add or remove multiple classes
-div.classList.add('foo', 'bar', 'baz');
-div.classList.remove('foo', 'bar', 'baz');
+// // add/remove visible, depending on test conditional, i less than 10
+// div.classList.toggle('visible', i < 10);
 
-// add or remove multiple classes using spread syntax
-const cls = ['foo', 'bar'];
-div.classList.add(...cls);
-div.classList.remove(...cls);
+// console.log(div.classList.contains('foo'));
 
-// replace class "foo" with class "bar"
-div.classList.replace('foo', 'bar');
+// // add or remove multiple classes
+// div.classList.add('foo', 'bar', 'baz');
+// div.classList.remove('foo', 'bar', 'baz');
+
+// // add or remove multiple classes using spread syntax
+// const cls = ['foo', 'bar'];
+// div.classList.add(...cls);
+// div.classList.remove(...cls);
+
+// // replace class "foo" with class "bar"
+// div.classList.replace('foo', 'bar');
+
+// //--------------------------------------------------//
+// let active = document.getElementsByClassName('your-active-class');
+
+// function update() {
+//   const container = document.getElementById('controls');
+//   const elem = document.getElementById('example');
+//   const rect = elem.getBoundingClientRect();
+
+//   container.innerHTML = '';
+//   for (const key in rect) {
+//     if (typeof rect[key] !== 'function') {
+//       let para = document.createElement('p');
+//       para.textContent = `${key} : ${rect[key]}`;
+//       container.appendChild(para);
+//     }
+//   }
+// }
+
+// document.addEventListener('scroll', update);
+// update();
