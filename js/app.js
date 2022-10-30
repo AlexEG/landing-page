cc6d1eae2353b595d6a3019cbb6ee752197c8ddd
Alexander EG#9070 <116421926+AlexEG@users.noreply.github.com>
2022-10-30 18:15:30 +0200

    Update app.js

--- js/app.js
+++ js/app.js
@@ -1,19 +1,13 @@
 //Navigation is built dynamically as an unordered list. Start with empty ul and dynamically build navigation using Append, appendChild, and innerHTML
 
+//stor value of navbar__list inside a var [done]
 let sections = document.querySelectorAll('section');
+//make a data structure of all sections in the page [done]
+let navbarList = document.getElementById('navbar__list');
 
-// testing if I can get the value of data-nav [section's title]
+// function navigationMenu() {
 
-function navigation() {
-  let sections = document.querySelectorAll('section');
-  //stor value of navbar__list inside a var
-
-  let navbarList = document.getElementById('navbar__list');
-
-  test2.innerHTML = sections;
-  //now let's index all sections inside ul element
-}
-navigation();
+// }
+// navigation();
 
 console.log(sections);
-//trying to save all
