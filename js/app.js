bfe353ed6344aa50241a47efdefe2f2feb1f1913
Alexander EG#9070 <116421926+AlexEG@users.noreply.github.com>
2022-10-30 18:35:46 +0200

    testing

--- js/app.js
+++ js/app.js
@@ -1,13 +1,21 @@
 //Navigation is built dynamically as an unordered list. Start with empty ul and dynamically build navigation using Append, appendChild, and innerHTML
 
 //stor value of navbar__list inside a var [done]
-let sections = document.querySelectorAll('section');
+let sections = Array.from(document.querySelectorAll('section'));
 //make a data structure of all sections in the page [done]
 let navbarList = document.getElementById('navbar__list');
 
-// function navigationMenu() {
+function navigationMenu() {
+  //loop over this data structure [done]
+  sections.forEach(element => {
+    //create an unorder list for each element in this structure [done]
+    createLi = document.createElement('li');
+    //make a html element with
+    createLi.innerText = 'hi'; //testing
+    navbarList.appendChild(createLi);
+  });
+}
 
-// }
-// navigation();
+navigationMenu();
 
 console.log(sections);
